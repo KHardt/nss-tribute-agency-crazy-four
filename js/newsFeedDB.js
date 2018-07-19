8c72225411ab80e83028a9a54768b19737eb55b0
Klaus Hardt <klausheina@gmail.com>
2018-07-19 09:32:15 -0500

    separated out DB files

---
+++ js/newsFeedDB.js
@@ -0,0 +1,38 @@
+const NicolasCageDB = {};
+
+NicolasCageDB.newsFeed = [];
+
+
+const events = [
+
+    {
+        date: "",
+        location: "",
+        image: "#",
+        type: "",
+        collaborators: "",
+    },
+    {
+        date: "",
+        location: "",
+        image: "#",
+        type: "",
+        collaborators: "",
+        
+    },
+    {
+        date: "",
+        location: "",
+        image: "#",
+        type: "",
+        collaborators: "",
+        
+    },
+
+]
+
+
+NicolasCageDB.newsFeed.push(events)
+
+
+console.log(NicolasCageDB.newsFeed)
