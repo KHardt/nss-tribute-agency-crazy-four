316b9f52016e8c4b2e743c2c70d9039e9c5264a6
priyankagargc27 <40770470+priyankagargc27@users.noreply.github.com>
2018-07-19 20:11:15 -0500

    changes commited

--- js/executive.js
+++ js/executive.js
@@ -9,19 +9,23 @@ console.log(loadDatabase("NicolasCageDB"));
 
 
 const container=document.getElementById("executivesummery")
-const portraitTitle=document.createElement("h2")
-portraitTitle.className="portraitHeader"
-portraitTitle.innerHTML=NicolasCageDB.execSummary.portrait.name
-container.appendChild(portraitTitle)
 
 const imgPortrait=document.createElement("img")
 imgPortrait.src=NicolasCageDB.execSummary.portrait.url
 container.appendChild(imgPortrait)
 
+
+const portraitTitle=document.createElement("h2")
+portraitTitle.className="portraitHeader"
+portraitTitle.innerHTML=`Name: ${NicolasCageDB.execSummary.portrait.name}`
+container.appendChild(portraitTitle)
+
+
+
 //const container=document.getElementById("executivesummery")
 const aliasNames=document.createElement("h4")
 aliasNames.className="Aliasheader"
-aliasNames.innerHTML=`${NicolasCageDB.execSummary.alias.First}${NicolasCageDB.execSummary.alias.Second}`
+aliasNames.innerHTML=`Alias: ${NicolasCageDB.execSummary.alias.First} ${NicolasCageDB.execSummary.alias.Second}`
 //aliasNames.innerHTML=NicolasCageDB.execSummary.alias.Second
 container.appendChild(aliasNames)
 
@@ -30,6 +34,11 @@ const collabNames = document.createElement("h4")
 collabNames.className="collabHeader"
 collabNames.innerHTML=`${NicolasCageDB.execSummary.collaborators.testperson1} ${NicolasCageDB.execSummary.collaborators.testperson2}`
 container.appendChild(collabNames)
+
+const currentRes=document.createElement("h4")
+currentRes.className="resHeader"
+currentRes.innerHTML=`${NicolasCageDB.execSummary.currentResidence.location} ${NicolasCageDB.execSummary.currentResidence.date}`
+container.appendChild(currentRes)
 
 
 
