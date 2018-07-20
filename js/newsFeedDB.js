86d8ab599a15065d1b67dca2808eb8220d36481c
Dejan Stjepanovic <ds2q88@gmail.com>
2018-07-20 09:13:52 -0500

    NewsFeed Database

--- js/newsFeedDB.js
+++ js/newsFeedDB.js
@@ -1,38 +1,80 @@
 const NicolasCageDB = {};
 
-NicolasCageDB.newsFeed = [];
-
-
-const events = [
+NicolasCageDB.newsFeed =[
 
     {
-        date: "",
-        location: "",
-        image: "#",
-        type: "",
-        collaborators: "",
+        name: "Chilling to the BONE!!!",
+        date: "December 2013",
+        location: "New York and Mongolia",
+        image: "../image/nic-cage-dinosaur-skull.jpg",
+        type: "Drama",
+        collaborators: "US attourney of Manhattan",
+        information: "Cage found himself in a pickle after bidding and buying a rare dinosaur skull. Soon thereafter the US attourney of Manhattan and the Mongolian government moved in to determine the skull as smuggled illegally to the U.S.A. Cage is not accused of wrong doing and has returned the piece. Nic paid $276,000 for the artifact.",
+        link: href = "https://www.independent.co.uk/arts-entertainment/films/news/nicolas-cage-to-return-stolen-dinosaur-skull-to-the-mongolian-government-a6784081.html"
     },
     {
-        date: "",
-        location: "",
-        image: "#",
-        type: "",
-        collaborators: "",
-        
+        name: "Name of news!",
+        date: "date2",
+        location: "location2",
+        image: "../image/cage-type-movies.png",
+        type: "type2",
+        collaborators: "collaborators",
+        information: "sdfsdfsdf sdafasdfs sdfsdfsdf sdffsdf",
+        link: href ="https://fivethirtyeight.com/features/the-five-types-of-nicolas-cage-movies/"
     },
     {
-        date: "",
-        location: "",
-        image: "#",
-        type: "",
-        collaborators: "",
-        
+        name: "Name of news!",
+        date: "date3",
+        location: "location3",
+        image: "../image/mandy-movie-nick.jpeg",
+        type: "type3",
+        collaborators: "collaborators",
+        information: "sdfsdfsdf sdafasdfs sdfsdfsdf sdffsdf",
+        link: href = "https://bloody-disgusting.com/movie/3510526/weve-got-fresh-batch-images-insane-nic-cage-film-mandy/"
     },
 
 ]
 
 
-NicolasCageDB.newsFeed.push(events)
 
 
-console.log(NicolasCageDB.newsFeed)
+
+
+// console.log(NicolasCageDB.newsFeed)
+
+// Persist the database to localStorage
+const saveDatabaseNews = function (databaseObjectNews, localStorageKeyNews) {
+    /*
+        Convert the Object into a string.
+    */
+   const stringifiedDatabaseNews = JSON.stringify(databaseObjectNews)
+
+   /*
+       Create a key in local storage, and store the string
+       version of your inventory database as the value
+   */
+   localStorage.setItem(localStorageKeyNews, stringifiedDatabaseNews)
+}
+saveDatabaseNews(NicolasCageDB.newsFeed, "NicolasCageDBNewsFeed")
+//saveDatabase(HomeInventoryDatabase, "HomeInventory")
+
+
+
+// const loadDatabaseNews = function (localStorageKeyNews) {
+//     // Get the string version of the database
+//     const databaseStringNews = localStorage.getItem(localStorageKeyNews)
+
+//     // Use JSON.parse() to convert the string back into an object
+//     return JSON.parse(databaseStringNews)
+// }
+// console.log(loadDatabaseNews("NicolasCageDBNewsFeed"));
+
+// //var Element = document.getElementById("page-container").innerHTML;
+
+// const loadDatabaseNews = function (localStorageKeyNews) {
+//     // Get the string version of the database
+//     const databaseStringNews = localStorage.getItem(localStorageKeyNews)
+
+//     // Use JSON.parse() to convert the string back into an object
+//     return JSON.parse(databaseStringNews)
+// }
